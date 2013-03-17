8849339a6cd44edce58c02adcd56cbea343a9023
Carlos E. Caballero B <cijkb.j@gmail.com>
2013-03-17 15:25:04 -0400

    reordenando el proyecto

--- index.php
+++ index.php
@@ -5,9 +5,4 @@ function __autoload($class) {
     include 'models/' . ucfirst(strtolower($class)) . '.php';
 }
 
-?>
-
-
-                <?php
-                echo '..' . $_GET['page'];
-                ?>
+echo '..' . $_GET['page'];
